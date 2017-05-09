d8a1296455aee5f45d35dc7b832352a40d2e0cb0
John <dlink4096@gmail.com>
2017-05-09 20:45:13 +0300

    Dissable blinking current day for non current months Add some comments for clarity.

--- calender.php
+++ calender.php
@@ -1,4 +1,4 @@
-<?php
+<?php	// calender.php - simple web calender; tested on php 7.0.1
 	$months=array(	'January'=>'',
 			'February'=>'',
 			'March'=>'',
@@ -10,14 +10,16 @@
 			'September'=>'',
 			'October'=>'',
 			'November'=>'',
-			'December'=>'');
-	$time=time();
+			'December'=>'');	// this array for forming input date form
+	$time=time();	// default time (month) - current time
 	$year='';
-	if(!empty($_POST['year']) && !empty($_POST['month']))
+	$current_month=1;	// check if we show current month then show current day blinking
+	if(!empty($_POST['year']) && !empty($_POST['month']))	// take year and month from input form, if they are 
 	{
 		$time=strtotime($_POST['month'].' '.$_POST['year']);
 		$year=$_POST['year'];
 		$months[$_POST['month']]='selected';
+		$current_month=0;
 	}
 ?>
 <!doctype html>
@@ -29,6 +31,7 @@
 	<h1>Calender</h1>
 </head>
 <body>
+<!-------------------------------INPUT FORM BEGIN---------------------------------------------->
 	<form method="POST">
 		<p>Enter year: <input type="text" name="year" size="4" value="<?= $year ?>"></p>
 		<p>Choose month:  <select name="month">
@@ -39,10 +42,12 @@
 					</select></p>
 		<input type="submit" value="Show">
 	</form>
+<!-------------------------------INPUT FORM END------------------------------------------------>
+	
 	<pre>
 	<?php
 // horizontal version
-	function get_wday1($mday, $wday)	// function calculate day of week of 1st day in month
+	function get_wday1($mday, $wday)	// function calculate day of week of 1st day in the month; return 1..7
 	{
 		$retval=$wday-($mday-floor($mday/7)*7)+1;
 		return $retval;
@@ -65,12 +70,12 @@
 			}
 			else if($day<=$tdays)
 			{
-				if($day==$mday) // today day
+				if($day==$mday && $current_month) // today day
 				{
 					print '<span class="blink">';
 				}
 				printf("%2d", $day);
-				if($day==$mday) // today day
+				if($day==$mday && $current_month) // today day
 				{
 					print '</span>';
 				}
@@ -88,7 +93,7 @@
 	print '</table>';
 	
 // vertical version
-	// calculate number of weeks(fulll and not full) in this month
+	// calculate number of weeks(full and not full) in this month
 	$weeks=floor($tdays/7);	// number of full weeks
 	$rem=$tdays%7; // number of days out of full week
 	if($rem>0)
@@ -116,13 +121,12 @@
 			}
 			else
 			{
-			//	printf("%2d", $i+$j*7);
-				if($i+$j*7==$mday) // today day
+				if($i+$j*7==$mday && $current_month) // today day
 				{
 					print '<span class="blink">';
 				}
 				printf("%2d", $i+$j*7);
-				if($i+$j*7==$mday) // today day
+				if($i+$j*7==$mday && $current_month) // today day
 				{
 					print '</span>';
 				}
